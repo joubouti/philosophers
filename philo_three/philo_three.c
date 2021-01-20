0ab70185cbf036203d2d5b36580e60ba14ee610e
Oussama Joubouti <oussamajoubouti@gmail.com>
2021-01-20 19:39:23 +0100

    finish

--- philo_three/philo_three.c
+++ philo_three/philo_three.c
@@ -6,7 +6,7 @@
 /*   By: ojoubout <ojoubout@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/01 18:20:44 by ojoubout          #+#    #+#             */
-/*   Updated: 2021/01/16 19:17:17 by ojoubout         ###   ########.fr       */
+/*   Updated: 2021/01/20 19:17:57 by ojoubout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,18 @@
 
 int	g_turn = 0;
 
-int ft_error(char *err)
-{
-    write(2, err, ft_strlen(err));
-    write(2, "\n", 1);
-    return (EXIT_FAILURE);
-}
-
 void	*ft_check_die(void *ptr)
 {
-    t_philo *philo;
-	micro_second_t mc;
-    philo = ptr;
+	t_philo				*philo;
+	t_micro_second_t	mc;
+
+	philo = ptr;
 	while (philo->stat != DONE)
 	{
 		mc = get_time_stamp();
-		if ((mc - philo->last_eat > time_to_die) && philo->stat != EATING)
+		if ((mc - philo->last_eat > g_time_to_die) && philo->stat != EATING)
 		{
-			// printf(" %lu %lu %lu %lu %d\n", mc, philo->last_eat, mc - philo->last_eat, time_to_die, mc - philo->last_eat > time_to_die);
-			
-			// char c = g_philos[i].stat + 48;
-			// write(1, &c, 1);
+			g_stat = false;
 			philo->stat = DIED;
 			ft_print_status(philo);
 			exit(2);
@@ -43,151 +34,71 @@ void	*ft_check_die(void *ptr)
 	return (NULL);
 }
 
-void    ft_think(t_philo *philo)
-{
-    philo->stat = THINKING;
-	ft_print_status(philo);
-    sem_wait(one_at_time);
-}
-
-void    ft_take_forks(t_philo *philo)
-{
-
-	sem_wait(forks_sem);
-	sem_wait(forks_sem);
-	philo->stat = TAKE_FORKS;
-	ft_print_status(philo);
-    sem_post(one_at_time);
-}
-
-void    ft_eat(t_philo *philo)
-{
-    philo->stat = EATING;
-	philo->nb_of_eat++;
-	ft_print_status(philo);
-	usleep(time_to_eat);
-	philo->last_eat = get_time_stamp();
-}
-
-void    ft_put_forks(t_philo *philo)
-{
-	philo->stat = PUTS_FORKS;
-	ft_print_status(philo);
-	sem_post(forks_sem);
-	sem_post(forks_sem);
-
-}
-
-void    ft_sleep(t_philo *philo)
-{
-    philo->stat = SLEEPING;
-	ft_print_status(philo);
-	usleep(time_to_sleep);
-}
-
 void	philosophers(void *ptr)
 {
-    t_philo *philo;
+	t_philo *philo;
 
-    philo = ptr;
+	philo = ptr;
 	if (pthread_create(&philo->thread, NULL, ft_check_die, philo))
 		exit(ft_error("pthread_create failed!"));
-    while (g_nb_of_eat == -1 || philo->nb_of_eat < g_nb_of_eat)
-    {
-        ft_think(philo);
-        ft_take_forks(philo);
-        ft_eat(philo);
-        ft_put_forks(philo);
-        ft_sleep(philo);
-    }
-	philo->stat = DONE;
+	while (g_stat && (g_nb_of_eat == -1 || philo->nb_of_eat < g_nb_of_eat))
+	{
+		ft_think(philo);
+		ft_take_forks(philo);
+		ft_eat(philo);
+		ft_put_forks(philo);
+		ft_sleep(philo);
+	}
+	ft_set_stat(philo, DONE);
 	exit(EXIT_SUCCESS);
 }
 
-int    init()
-{
-    int i;
-
-    if (!(g_philos = malloc(sizeof(t_philo) * nb_of_philos)))
-        return (ft_error("malloc failed!"));
-    i = 0;
-	sem_unlink("forks_sem");
-	sem_unlink("one_at_time");
-	sem_unlink("print_sem");
-    if ((forks_sem = sem_open("forks_sem", O_CREAT, S_IRWXG, nb_of_philos)) == SEM_FAILED ||
-	(one_at_time = sem_open("one_at_time", O_CREAT, S_IRWXG, 1)) == SEM_FAILED ||
-	(print_sem = sem_open("print_sem", O_CREAT, S_IRWXG, 1)) == SEM_FAILED)
-    {
-        return (ft_error("failed to open semaphore"));
-    }
-
-	i = 0;
-	g_start_time = get_time_stamp();
-	printf("%lu\n", g_start_time);
-    while (i < nb_of_philos)
-    {
-        g_philos[i].id = i + 1;
-        g_philos[i].stat = -1;
-		g_philos[i].nb_of_eat = 0;
-        g_philos[i].last_eat = g_start_time;
-		if ((g_philos[i].pid = fork()) == 0)
-			philosophers(&g_philos[i]);
-		else if (g_philos[i].pid < 0)
-            return (ft_error("fork failed!"));
-        // if (pthread_create(&g_philos[i].thread, NULL, philosophers, &g_philos[i]))
-        //     return (ft_error("pthread_create failed!"));
-        i++;
-    }
-    return (EXIT_SUCCESS);
-}
-
-int    run()
+int		run(void)
 {
 	int	i;
+	int j;
 	int	status;
 
 	i = 0;
-	while (i < nb_of_philos)
+	while (i < g_nb_of_philos)
 	{
 		waitpid(-1, &status, 0);
 		if (WEXITSTATUS(status) == 2)
 		{
-			int i = 0;
-			while (i < nb_of_philos)
-				kill(g_philos[i++].pid, 9);
+			j = 0;
+			while (j < g_nb_of_philos)
+				kill(g_philos[j++].pid, 9);
 			return (EXIT_SUCCESS);
 		}
+		i++;
 	}
-
-    return (EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
 
-int    finalize()
+int		finalize(void)
 {
-	sem_unlink("forks_sem");
-	sem_unlink("one_at_time");
-	sem_unlink("print_sem");
+	sem_unlink("g_forks_sem");
+	sem_unlink("g_one_at_time");
+	sem_unlink("g_print_sem");
 	free(g_philos);
-    return (EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
 
-int main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
-	// argv = NULL;
 	if (argc != 5 && argc != 6)
 		return (ft_error("usage: ./philo_one number_of_philosophers"
-		"time_to_die time_to_eat time_to_sleep "
-		 "[number_of_times_each_philosopher_must_eat]"));
-	nb_of_philos = ft_atoi(argv[1]);
-	time_to_die = ft_atoi(argv[2]) * 1000;
-	time_to_eat = ft_atoi(argv[3]) * 1000;
-	time_to_sleep = ft_atoi(argv[4]) * 1000;
-	// printf("%lu\n", time_to_die);
+		"g_time_to_die g_time_to_eat g_time_to_sleep "
+		"[number_of_times_each_philosopher_must_eat]"));
+	g_nb_of_philos = ft_atoi(argv[1]);
+	g_time_to_die = ft_atoi(argv[2]) * 1000;
+	g_time_to_eat = ft_atoi(argv[3]) * 1000;
+	g_time_to_sleep = ft_atoi(argv[4]) * 1000;
 	g_nb_of_eat = (argc == 6) ? ft_atoi(argv[5]) : -1;
-	if (nb_of_philos < 0 || time_to_die < 0 || time_to_eat < 0 ||
-	time_to_sleep < 0 || g_nb_of_eat < -1)
-        return (EXIT_FAILURE);
-    if (init() || run() || finalize())
-        return (EXIT_FAILURE);
-    return (EXIT_SUCCESS);
+	if (g_nb_of_philos < 0 || g_time_to_die < 0 || g_time_to_eat < 0 ||
+	g_time_to_sleep < 0 || g_nb_of_eat < -1)
+		return (EXIT_FAILURE);
+	if (init() || run() || finalize())
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
 }
--- philo_three/philo_three.h
+++ philo_three/philo_three.h
@@ -6,7 +6,7 @@
 /*   By: ojoubout <ojoubout@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/01 18:20:51 by ojoubout          #+#    #+#             */
-/*   Updated: 2021/01/16 19:15:14 by ojoubout         ###   ########.fr       */
+/*   Updated: 2021/01/20 19:22:51 by ojoubout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,9 @@
 # include <sys/time.h>
 # include <string.h>
 # include <semaphore.h>
+# include <sys/fcntl.h>
 # include <signal.h>
+# include <sys/wait.h>
 # define DIED		0
 # define THINKING	1
 # define TAKE_FORKS	2
@@ -29,37 +31,48 @@
 # define DONE		6
 # define BUFF_SIZE	64
 
+typedef enum {
+	false,
+	true
+}	t_bool;
 
-typedef enum { false, true } bool;
+typedef long	t_micro_second_t;
 
-typedef long   micro_second_t;
-
-typedef struct	s_philo
+typedef struct			s_philo
 {
-	pthread_t		thread;
-	int				id;
-	int				stat;
-	int				nb_of_eat;
-	micro_second_t	last_eat;
-	pid_t			pid;
-}				t_philo;
+	pthread_t			thread;
+	int					id;
+	int					stat;
+	int					nb_of_eat;
+	t_micro_second_t	last_eat;
+	pid_t				pid;
+}						t_philo;
 
-sem_t	*forks_sem;
-sem_t	*one_at_time;
-// pthread_mutex_t	die_mutex;
-sem_t	*print_sem;
-t_philo			*g_philos;
-int				nb_of_philos;
-micro_second_t	time_to_die;
-micro_second_t	time_to_eat;
-micro_second_t	time_to_sleep;
-int				g_nb_of_eat;
-micro_second_t	g_start_time;
+sem_t					*g_forks_sem;
+sem_t					*g_one_at_time;
+sem_t					*g_print_sem;
+t_philo					*g_philos;
+int						g_nb_of_philos;
+t_micro_second_t		g_time_to_die;
+t_micro_second_t		g_time_to_eat;
+t_micro_second_t		g_time_to_sleep;
+int						g_nb_of_eat;
+t_micro_second_t		g_start_time;
+t_bool					g_stat;
 
-size_t			ft_strlen(const char *s);
-micro_second_t	get_time_stamp();
-void			ft_print_status(t_philo *philo);
-void			*ft_bzero(void *s, size_t n);
-int				ft_atoi(const char *str);
+size_t					ft_strlen(const char *s);
+t_micro_second_t		get_time_stamp();
+void					ft_print_status(t_philo *philo);
+void					*ft_bzero(void *s, size_t n);
+int						ft_atoi(const char *str);
+int						init(void);
+void					ft_sleep(t_philo *philo);
+void					ft_put_forks(t_philo *philo);
+void					ft_eat(t_philo *philo);
+void					ft_take_forks(t_philo *philo);
+void					ft_think(t_philo *philo);
+void					ft_set_stat(t_philo *philo, int stat);
+int						ft_error(char *err);
+void					philosophers(void *ptr);
 
 #endif
